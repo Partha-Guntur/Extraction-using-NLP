c1a23844365294f00919c72592f7f6b9722cce2c
Partha Guntur <parthagunturu2003@gmail.com>
2023-07-16 21:52:35 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from website import create_app
+from flask import Flask
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# Asthama, Wheezing, Montelukast
+# Hypertension,	Headache, Lisinopril
+# Arthritis, Swelling, Naproxen
+# Anxiety, Nervousness, Alprazolam
+# Tuberculosis, Cough, Isoniazid
